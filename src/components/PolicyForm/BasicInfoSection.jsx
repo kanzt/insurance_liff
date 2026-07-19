6cf23a0c2291ac33a5d75f8806f0da05244b7d94
Kantaphat Tavelertsopon <kantaphat.tavelertsopon@krungsrinimble.com>
2026-07-19 16:21:29 +0700

    feat: update duplicate warning message to be year specific

--- src/components/PolicyForm/BasicInfoSection.jsx
+++ src/components/PolicyForm/BasicInfoSection.jsx
@@ -139,7 +139,7 @@ export function BasicInfoSection({
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 shrink-0 text-red-500" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
                   </svg>
-                  <span><strong>พบข้อมูลนี้ในระบบแล้ว!</strong> ไม่สามารถสร้างเช็คเบี้ยใหม่ได้</span>
+                  <span><strong>เคยขอใบเสนอราคาในปีนี้แล้ว!</strong> ไม่สามารถสร้างเช็คเบี้ยใหม่ซ้ำได้</span>
                 </div>
                 <button
                   type="button"
